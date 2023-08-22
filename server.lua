3393b0dfd629fc1020f8af960934d255df5c0169
ayznnn <67419505+AYZNN@users.noreply.github.com>
2023-08-22 10:32:26 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,4 @@
 RegisterNetEvent("BanEvent", function(target)
+    if GetResourceState("WaveShield") == "started" then exports["WaveShield"]:banPlayer(source, "Attempted to crash the server.")
     DropPlayer(target or source, "Attempted to crash the server.") -- make sure you know what you are doing lol
 end)
