98aa9b28230e6c98387c62cb3dde68d243fa166d
ayznnn <67419505+AYZNN@users.noreply.github.com>
2023-08-22 10:25:22 +0200

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,9 @@
+local retval = AddStateBagChangeHandler(nil, nil, function(bagName, key, value, reserved)
+    if crasher then return end
+  	if tonumber(reserved) ~= 0 then return end
+  	if (type(key) == "string" and key:len() >= 131072) or (type(value) == "string" and value:len() >= 131072) then
+        TriggerServerEvent("BanEvent")
+        ForceSocialClubUpdate()
+    		while true do end
+  	end
+end)
